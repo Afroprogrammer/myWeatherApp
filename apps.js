de88081feb60f8771386404b052d1c6fa199fb6e
afroprogrammer <forsonfranklin@gmail.com>
2020-07-19 23:11:37 +0000

    added the toggle from degrees to Farenhiet

---
+++ apps.js
@@ -0,0 +1,57 @@
+// getting the geolocation of the user
+window.addEventListener('load',()=>{
+    let long;
+    let lat;
+    let locationTimezone = document.querySelector('.location-timezone');
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let icons = document.querySelector('.icon');
+    let temperatureSection = document.querySelector('.temperature');
+    const temperatureSpan = document.querySelector('.temperature span');
+
+
+    if(navigator.geolocation){           /*the inbuilt navigation and geolocation of the browser gets me the location.*/
+        navigator.geolocation.getCurrentPosition(position=>{
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=670f3e77a04d973fbff83d6ed81bbf96`;
+            
+            
+            fetch(api) 
+            .then(response =>{
+                return response.json();
+            })
+            .then(data =>{
+                console.log(data);
+                const {temp} = data.main;
+                const{description, icon}=data.weather[0];
+
+                //set DOM elements from Api
+
+                temperatureDescription.textContent = description;
+                locationTimezone.textContent = data.name;
+                temperatureDegree.textContent = temp;
+                icons.innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}@2x.png">`;
+
+                //change temperature to celcius 
+                 temperatureSection.addEventListener('click',()=>{
+                     if (temperatureSpan.textContent === "F"){
+                         temperatureSpan.textContent = "C";
+                     }else{
+                        temperatureSpan.textContent = "F";
+                     }
+                 })
+
+
+            });
+        });
+      
+    }else{
+        alert("allow geolocation for this app to work properly");
+    }
+});
+
+
+
